a8052ca0982e355e8f5c5c50cee412d8588fc591
Chris Wright <christopher.wright47@gmail.com>
2014-02-27 11:01:05 -0800

    added deleting user capabilities for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    User.find(params[:id]).destroy
+    redirect_to root_path
   end
 
   private
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Team do
+  let(:name) { 'Lincoln' }
+  let(:mascot) { 'Cardinals' }
   before do
-    @team = FactoryGirl.create(:team)
+    @team = FactoryGirl.create(:team, name: name, mascot: mascot)
     @athlete = FactoryGirl.create(:user, team: @team)
     @coach = FactoryGirl.create(:user, coach: true, team: @team)
   end
@@ -11,21 +13,23 @@ describe Team do
     expect(@team).to be_valid
   end
 
-  it 'should have the right name' do
-    expect(@team.name).to eq 'Lincoln'
+  it 'should have a name' do
+    expect(@team.name).to eq name
   end
 
-  it 'should have the right mascot' do
-    expect(@team.mascot).to eq 'Cardinals'
+  it 'should have a mascot' do
+    expect(@team.mascot).to eq mascot
   end
 
   it 'should have a collection of athletes' do
     expect(@athlete.team).to eq @team
+    expect(@team.athletes).to_not include(@coach)
     expect(@team.athletes).to include(@athlete)
   end
 
   it 'should have a collection of coaches' do
     expect(@coach.team).to eq @team
+    expect(@team.coaches).to_not include(@athlete)
     expect(@team.coaches).to include(@coach)
   end
 
